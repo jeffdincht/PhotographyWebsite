ff17c59a17080cb2437e56564fc9420b53d4f595
Gardenia Oliveira <jobs.gardeniaoliveira@gmail.com>
2023-10-14 23:51:10 +0100

    created gallery

--- project/src/app/components/grid/grid.component.ts
+++ project/src/app/components/grid/grid.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.scss']
+  styleUrls: ['./grid.component.scss'],
 })
 export class GridComponent {
-
+  @Input() image: string = '';
 }
--- project/src/app/pages/family/family.component.ts
+++ project/src/app/pages/family/family.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Image } from 'src/app/Models/images.models';
 
 @Component({
   selector: 'app-family',
   templateUrl: './family.component.html',
-  styleUrls: ['./family.component.scss']
+  styleUrls: ['./family.component.scss'],
 })
 export class FamilyComponent {
-
+  images: Image[] = [
+    {
+      image: '../../../assets/images/familia/6meses_43.jpg',
+    },
+    {
+      image: '../../../assets/images/familia/6meses_47.jpg',
+    },
+    {
+      image: '../../../assets/images/familia/6meses_49.jpg',
+    },
+  ];
 }
